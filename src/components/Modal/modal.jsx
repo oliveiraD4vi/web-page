88e57aa87ab74ff067f26fc6c5e92ac7e12e74a5
Davi Oliveira <davi.comp.oliveira@gmail.com>
2023-02-01 09:02:11 -0300

    feat(components): added basic modal component to the project
    This component will be used for the contact information after

---
+++ src/components/Modal/modal.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+import "./modal.css";
+
+const Modal = ({ children, title, btn }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div>
+      <button onClick={handleToggle}>{btn ? btn : 'Open Modal'}</button>
+      {isOpen && (
+        <div className="wrapper">
+          <div className="modal">
+            <div className="body">
+              <div className="header">
+                <p>{title}</p>
+                <button onClick={handleToggle} className="close-btn">
+                  X
+                </button>
+              </div>
+              <div className="content">{children}</div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Modal;
